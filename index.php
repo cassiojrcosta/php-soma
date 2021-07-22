d158418e78f36aba2c911b29ddee42000707d43c
Leonardo Faleiros <cassiojunior1220@gmail.com>
2021-07-22 19:08:19 -0300

    adicionei foto

--- index.php
+++ index.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-image: url(https://images.unsplash.com/photo-1518133910546-b6c2fb7d79e3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80);
+        }
+
+        form{
+            height: 150px;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+            background-color: #bbb;
+            border-radius: 18px;
+        }
+    </style>
 </head>
 <body>
-  <form action="soma.php" method="post"> 
-  <input type="number" name="num1">
-  <input type="number" name="num2">
-  <button type="submit">Somar</button>
-  </form>
-</body>
-</html>
+    <form action="soma.php" method="post">
+    <input type="number" name=" num1">
+    <input type="number" name=" num2">
+    <button type="submit">somar</button>
+    </form>
---
+++ soma.php
@@ -0,0 +1,11 @@
+<?php
+
+$num1 = $_POST['NUM1'];
+$num2 = $_POST['NUM2'];
+
+$SOMA = $NUM1 + $NUM2;
+
+
+echo $soma;
+
+?>
